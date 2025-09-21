590a6c3a2d06bbbd99a34c9046169663b2e7a4cf
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2025-09-21 04:01:31 -0300

    feat(translations): add Spanish translation (#97)

--- src/translations/translations.ts
+++ src/translations/translations.ts
@@ -1,6 +1,8 @@
 import englishTranslation from '../translations/english.json';
 import polishTranslation from '../translations/polish.json';
 import germanTranslation from '../translations/german.json';
+import ptbrTranslation from '../translations/pt-br.json';
+import spanishTranslation from '../translations/spanish.json';
 
 export interface Language {
   id: string;
@@ -12,6 +14,8 @@ export const languages: Language[] = [
   { id: 'en', name: 'English', translations: englishTranslation },
   { id: 'pl', name: 'Polski', translations: polishTranslation },
   { id: 'de', name: 'Deutsch', translations: germanTranslation },
+  { id: 'pt-br', name: 'Português (Brasil)', translations: ptbrTranslation },
+  { id: 'es', name: 'Español', translations: spanishTranslation },
 ];
 
 export function tl(language: Language, text: string, params?: string[]) {
